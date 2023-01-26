8588554557c002de0d4e0a256ade7e0490c3c80d
Shaik Wasef <wasef@regology.com>
2023-01-26 18:35:55 -0500

    feat : Add cors middleware to api

--- border-express/src/index.ts
+++ border-express/src/index.ts
@@ -1,12 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { questions } from './routes/routes';
+import cors from 'cors'
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.use('/questions', questions);
 
 app.listen(port, () => {
